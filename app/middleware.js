bac7572882541606d5163e81d99c973eeab5475d
BRIKIAchraf <brikiachraf@DJERBA.r-iset.tn>
2024-05-26 22:13:09 +0100

    Release 1:this is the first version of production testing

--- app/middleware.js
+++ app/middleware.js
@@ -20,7 +20,7 @@ const middleware = [
   helmet(), // set security HTTP headers
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "http://localhost:3003"],
+    origin: ["http://localhost:3003","https://SchoolOmegup.com"],
     optionsSuccessStatus: 200,
   }),
   express.json(),
